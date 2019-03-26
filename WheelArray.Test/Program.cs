453bc4a67d71ee24443ef27d4f8496c26c1c3318
Shems Boukhatem <shems.boukhatem@lavenpartners.com>
2019-03-26 16:49:36 +0000

    checking in

---
+++ WheelArray.Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace WheelArray.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = Enum.GetValues(typeof(Months)).Cast<Months>();
+
+            var ar = new WheelArray<Months>(values.ToArray());
+
+            ar.SetStart(1);
+            foreach(var a in ar)
+            {
+                Console.WriteLine($"{a}: {(int)a}");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    enum Months
+    {
+        Jan = 1,
+        Feb,
+        Mar,
+        Apr,
+        May,
+        Jun,
+        Jul,
+        Aug,
+        Sep,
+        Oct,
+        Nov,
+        Dec
+    }
+}
---
+++ WheelArray/WheelArray.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+
+namespace WheelArray
+{
+    public class WheelArray<T> : IEnumerator, IEnumerable where T : struct, IConvertible, IComparable, IFormattable
+    {
+        private T[] _array;
+        int position = -1;
+
+        public object Current => _array[position];
+
+        public WheelArray(T[] array)
+        {
+            _array = array;
+        }
+
+        public void SetStart(int index)
+        {
+            if (index >= _array.Length || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var newArray = new T[_array.Length];
+            var newIndex = 0;
+
+            for (var i = index; i < _array.Length; i++)
+            {
+                newArray[newIndex] = _array[i];
+                newIndex++;
+            }
+
+            for (var i = 0; i < index; i++)
+            {
+                newArray[newIndex] = _array[i];
+                newIndex++;
+            }
+
+            _array = newArray;
+        }
+
+        public T this[int i]
+        {
+            get { return _array[i]; }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return position < _array.Length;
+        }
+
+        public void Reset()
+        {
+            position = 0;
+        }
+    }
+}
